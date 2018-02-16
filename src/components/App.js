6226fcb5a19377ade4638dc68ad2f93141ec55f6
Alejandro Sanchez <alejandro.sanchez@oxid-esales.com>
2018-02-16 15:47:37 +0100

    Added Helmet and Surge :football:

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 import FullScreenLoaidng from './FullScreenLoaidng';
 import Main from './Main';
 import Footer from './Footer';
 
+import globalStyles from './global-styles';
+
+globalStyles();
+
 const App = () => (
   <div>
+    <Helmet title="You Are Doing Great" />
+
     <FullScreenLoaidng />
     <Main />
     <Footer />
